38c3881d748db5457d8c70680ef0cae034711216
Kelvin S <kelvinnothere@gmail.com>
2026-09-04 11:09:33 +0800

    Activity 4

--- hello.py
+++ hello.py
@@ -18,7 +18,18 @@ followers += 50
 print("Day 1:", followers)
 
 followers += 20
-print("Day 1:", followers)
+print("Day 2:", followers)
 
 followers -= 10
-print("Day 1:", followers)
+print("Day 3:", followers)
+
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("=====================")
+print("Username: ", username)
+print("Age: ", age)
+print("Category: ", category)
