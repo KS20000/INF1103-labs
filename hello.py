03a90dbdade824aaad9537b2f174a3b5a23a0a6c
Kelvin S <kelvinnothere@gmail.com>
2026-09-04 11:03:57 +0800

    Activity 3

--- hello.py
+++ hello.py
@@ -6,8 +6,19 @@ print("==============================")
 
 username = "cool_creator"
 bio = "Fun blogger"
-followers = "1000"
+followers = 1000
+
 
 print("Username: ", username)
 print("Bio: ", bio)
 print("Followers: ", followers)
+
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 1:", followers)
+
+followers -= 10
+print("Day 1:", followers)
